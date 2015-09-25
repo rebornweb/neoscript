69b33695b7cfe4c937a5e6e1d53cb57fb1e87c5a
rebornweb <andrei@rebornweb.co.nz>
2015-09-25 23:41:47 +1200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# neoscript
+
+This is A Repo of Andrei Nicolas
+//Check PHP and JavaScript Examples
+
+Who Manages
+#http://Rebornweb.co.nz "Swiftly Building Websites"
+#The Bella Theme is a General SASS/HTML5 Theme to start with 
+
+Working Demo of the simple Bella put in action for a E-commerce website at
+http://bella.rebornweb.co.nz but i changed the header and the navigation on this site.
+
+#What's here is just the 'simple version of the Bella Theme'
+
+Which is Responsive and Has a "Hamburger" and Dropdown menu Nav Using SASS/css,
+I recommend you switch to using a SASS compiler which generates CSS if you haven't
+already :)
+
+'I said a 10 Second car, not a 10 Minute Car'
+
+
+
+This Repo Offers Free SilverStripe Themes/Mysite Code For the World to Enjoy There is a Stop Watch 
+and Metronome working for the world to tinker with.
+
+You just have to do A /dev/build and ?flush=all after adding in the theme and mysite code.
+
+#There is Also a Waving Animation Waving.js
+
+Wishing you guys would fork it play with it as you wish,
+it is fun, also a nightmare :) it waves a whole line in a anchor wrapped with
+any element with the class of class='wavingwrap' 
+
+Link to demo is at http://andrei.rebornweb.co.nz/hi
+
+if you want the Waving.js go to wavingcssanimations folder :)
+
+Download the Whole Zip or Git clone
+
+
+
+The CSS3 compiliation on there is from Callmenick.com so Thanks to that Guy :)  
+
+Enjoy
---
+++ wavingcssanimations/js/waving.js
@@ -0,0 +1,129 @@
+ $(function(){
+
+ /**
+  *05/09/15
+  *Made by Andrei Nicolas
+  *Rebornweb.co.nz still at Beta test
+  *Fork if you wish
+  * Usage: it needs to be Wrapped in a  div or
+       any element with class='wavingwrap'
+       and with an anchor inside then content that waves.
+       Matthew 5:16
+  *
+  */
+ 
+ 
+ //Bug if first anchor isnt the longest will spas out 
+   var wavingElements = $(".wavingwrap div");
+  var strsplit = $('.wavingwrap a').html(); 
+ 
+  var wavingAnchors = $(".wavingwrap a");
+  
+  wavingAnchors.find('div').parent().contents().filter(function() {
+      return this.nodeType === 3;
+    }).remove();
+    
+  
+ var TotalLen = wavingAnchors.text().length;
+   var a1len = wavingAnchors.eq(0).text().length;
+    var a2len = wavingAnchors.eq(1).text().length;
+   var a3len = wavingAnchors.eq(2).text().length;
+var a4len = wavingAnchors.eq(3).text().length;
+var maxLen = -50; //Max Length Slice
+
+var ahref = $('.wavingwrap a').attr('href');
+
+  function mrLoopy() {
+  
+   var wavingElements = $(".wavingwrap div");
+   var str = $('.wavingwrap a').text();
+   var incrementUp = '-10px';
+   var incrementDown = '-2px';
+   var mechUps = {'transform':'translate3d(0px,'+ incrementUp +',0px)','Moztransform':'translate3d(0px,'+ incrementUp +',0px)','WebkitTransform':'translate3d(0px,'+ incrementUp +',0px)','msTransform':'translate3d(0px,'+ incrementUp +',0px)'};
+   var mechDowns = {'transform':'translate3d(0px,'+ incrementDown +',0px)','Moztransform':'translate3d(0px,'+ incrementDown +',0px)','WebkitTransform':'translate3d(0px,'+ incrementDown +',0px)','msTransform':'translate3d(0px,'+ incrementDown +',0px)'};
+   var time = 100;
+   var waveSpeed = 30;
+   
+for (var i=0; i<str.length; i++) {
+(function(i){
+    setTimeout(function(){
+     wavingElements.eq(i).css(mechUps);
+ 
+    setTimeout(function(){
+      wavingElements.eq(i).css(mechDowns);
+           },time);
+      
+
+    }, i * waveSpeed);
+
+  }(i));
+
+}//End of Infamous for loop
+
+  }// End of Mr Loopy
+  
+
+    /* if stuffs up put back in Removes the Anchor Text
+    $(this).find('div').parent().contents().filter(function() {
+      return this.nodeType === 3;
+    }).remove();*/
+   
+    
+    
+ $(".wavingwrap li ").hover( 
+    
+    function (){ //On mouseover
+
+//Removes the Anchor Text
+   $(this).find('div').parent().contents().filter(function() {
+      return this.nodeType === 3;
+    }).remove();
+ 
+
+       var anchorage = $(this).find('a');
+      var  ant = anchorage.text();
+      var anchorlength = ant.length;
+      var seldiv = $('.wavingwrap a div');
+      var divl = seldiv.length;
+    
+    
+             anchorage.each(function(i){
+     /*
+ For testing purposes          
+   console.log('anchorlength: ' +anchorlength + ' div length: ' + divl);        
+           */
+        
+  for (var e = 0; e < ant.length;  e ++) {
+
+$(this).append('<div class="append" style="display:inline-block;"> '+ ant[e] + '</div>');
+        }
+     
+      });
+     
+   mrLoopy();
+
+  //Wraps and hides the Anchor Text
+$(this).find('div').parent().contents().filter(function() {
+      return this.nodeType === 3;
+    }).wrap( "<span style='display:none;'></span>" );
+
+$(this).find('span').css('display','none');
+
+
+ }, //End mouseover
+ 
+    function() {//On mouseleave
+      
+ //Wraps and hides the Anchor Text
+$(this).find('span').css('display','block');
+     $(this).find('div').slice(maxLen).remove();
+         
+        }
+      
+      ) //End of Hover Bracket  
+
+      $(".wavingwrap a").click(function(){mrLoopy();});
+
+      
+   
+});//END script
