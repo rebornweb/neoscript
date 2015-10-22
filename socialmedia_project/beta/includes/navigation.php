242ce95dfae3fb3f079121a36e9c5153cc99fae2
rebornweb <andrei@rebornweb.co.nz>
2015-10-22 17:20:38 +1300

    Social Media PHP/Mysql Enjoy

--- socialmedia_project/beta/includes/navigation.php
+++ socialmedia_project/beta/includes/navigation.php
@@ -1,7 +1,8 @@
 <nav>
 <ul>
-<li><a href='index.php'>Home</a> </li>
-<li><a href='registered.php'>Portfolio</a> </li>
+<li><a href='index.php'>Portfolio</a> </li>
+<li><a href='registered.php'>Home</a> </li>
+<li><a href='?logoff'>Logoff</a> </li>
 </ul>
 
 </nav>
