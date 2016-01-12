fbea479c46507a00472a1392a775d01cfdf46e19
rebornweb <andrei@rebornweb.co.nz>
2016-01-12 18:03:35 +1300

    preformance enchance

--- wavingcssanimations/js/waving.js
+++ wavingcssanimations/js/waving.js
@@ -19,12 +19,15 @@
  
  
 
-
-jQuery.fn.wavingPlugin = function(){
+ 
+var wavingList = function (test) {
   //Global Variable Block
+  this.test = test;
   var wavingElements = $(".wavingwrap div"),
+  listLi = $('.wavingwrap li'),
   strsplit = $('.wavingwrap a').html(),
   wavingAnchors = $(".wavingwrap a");
+  listLi.css('list-style','none');
   
   wavingAnchors.find('div').parent().contents().filter(function() {
       return this.nodeType === 3;
@@ -33,7 +36,6 @@ jQuery.fn.wavingPlugin = function(){
 
 var maxLen = -50; //Max Length Slice
 
-var ahref = $('.wavingwrap a').attr('href');
 
   function mrLoopy() {
   //Variable Block
@@ -67,8 +69,10 @@ for (var i=0; i<str.length; i++) {
 
 
     
- $(".wavingwrap li ").hover( 
-    function (){ //On mouseover
+var list = $(".wavingwrap li ");
+ 
+ list.hover( 
+     function (){ //On mouseover
 
 
 //Variable Block
@@ -89,7 +93,7 @@ $(this).append('<div class="append" style="display:inline-block;"> '+ ant[e] + '
      
       });
      
-   mrLoopy();
+   obj.animation();
 
   //Wraps and hides the Anchor Text
 $(this).find('div').parent().contents().filter(function(){
@@ -100,23 +104,65 @@ $(this).find('span').css('display','none');
 
  }, //End mouseover
  
-    function() {//On mouseleave
+ function() {//On mouseleave
       
  //Wraps and hides the Anchor Text
 $(this).find('span').css('display','block');
-//$(this).find('div').slice(maxLen).remove();
+$(this).find('div').slice(maxLen).remove();
          
         }
       
       ) //End of Hover Bracket  
 
-      $(".wavingwrap a").click(function(){mrLoopy();});
+      $(".wavingwrap a").click(function(){obj.animation();});
 
 };
 
 //Makes .wavingwrap a have div animation 
-$('.wavingwrap').wavingPlugin();
+//$('.wavingwrap').wavingPlugin();
+
+ var wavingAni = function (test) {
+  wavingList.call(this,test);
+
+    
+         console.log(this.test); 
+ }
   
+  
+    wavingAni.prototype = Object.create(wavingList.prototype);
+  
+    wavingAni.prototype.animation= function(test) {
+    //Variable Block
+   var wavingElements = $(".wavingwrap div"),
+    str = $('.wavingwrap a').text(),
+    incrementUp = '-10px',
+    incrementDown = '-2px',
+    mechUps = {'transform':'translate3d(0px,'+ incrementUp +',0px)','Moztransform':'translate3d(0px,'+ incrementUp +',0px)','WebkitTransform':'translate3d(0px,'+ incrementUp +',0px)','msTransform':'translate3d(0px,'+ incrementUp +',0px)'},
+    mechDowns = {'transform':'translate3d(0px,'+ incrementDown +',0px)','Moztransform':'translate3d(0px,'+ incrementDown +',0px)','WebkitTransform':'translate3d(0px,'+ incrementDown +',0px)','msTransform':'translate3d(0px,'+ incrementDown +',0px)'},
+    time = 100,
+    waveSpeed = 30;
+   
+for (var i=0; i<str.length; i++) {
+(function(i){
+    setTimeout(function(){
+     wavingElements.eq(i).css(mechUps);
+ 
+    setTimeout(function(){
+      wavingElements.eq(i).css(mechDowns);
+           },time);
+      
+
+    }, i * waveSpeed);
+
+  }(i));
+
+}//End of Infamous for loop    
+      
+      
+
+    }
+  
+  var obj = new wavingAni('hello');
 
 /*Performance Testing
  var iterations = 1000;
