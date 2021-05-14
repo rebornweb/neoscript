76f99a972c6886c679ebc71f18f4bfa8d991efe5
rebornweb <andrei.nicolas@hotmail.com>
2021-05-14 15:14:33 +1200

    update

--- vue-fastingtracker-project/fasting-tracker/src/App.vue
+++ vue-fastingtracker-project/fasting-tracker/src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>I am vue</h1>
+    <FastTracker />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FastTracker from "./components/FastTracker";
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
-  }
-}
+    FastTracker,
+  },
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
