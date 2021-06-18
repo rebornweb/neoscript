8821b9124b491d1c67e21e3db8338b661afc4e82
Reborn Web <andrei.nicolas@hotmail.com>
2021-06-18 12:50:22 +1200

    New JS Play

---
+++ javascriptPlay/Destructuring data/config.js
@@ -0,0 +1,52 @@
+
+
+
+const info = {
+	"random": 38,
+	"random float": 56.768,
+	"bool": false,
+	"date": "1986-11-13",
+	"regEx": "helloooooooooooooooooo world",
+	"enum": "json",
+	"firstname": "Cassondra",
+	"lastname": "Marijo",
+	"city": "Bridgetown",
+	"country": "Faroe Islands",
+	"countryCode": "AE",
+	"email uses current data": "Cassondra.Marijo@gmail.com",
+	"email from expression": "Cassondra.Marijo@yopmail.com",
+    "companygroup": {
+        "nek":"ESR",
+        min:"EPA"
+
+    },
+
+	"array": [
+		"Gabriellia",
+		"Paule",
+		"Eve",
+		"Maryellen",
+		"Morganica"
+	],
+	"array of objects": [
+		{
+			"index": 0,
+			"index start at 5": 5
+		},
+		{
+			"index": 1,
+			"index start at 5": 6
+		},
+		{
+			"index": 2,
+			"index start at 5": 7
+		}
+	],
+	"Chandra": {
+		"age": 90
+	}
+}
+
+const {firstname, lastname, city, companygroup: {min} } = info;
+
+console.log(`i am nek minnit ${firstname} I work from 9 to 5 in ${city} at ${nek} `);
