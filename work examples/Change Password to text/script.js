65ce514d451349d5b1ab61c4bc98da1faff15eb8
Andrei <andrei.nicolas@hotmail.com>
2023-03-10 11:30:22 +1300

    Added work example to change a password to text and hide

---
+++ work examples/Change Password to text/script.js
@@ -0,0 +1,5 @@
+var pinToText = function () {
+    document.getElementById("newSecretPIN").setAttribute("type", "text");
+    document.getElementById("reenterSecretPIN").setAttribute("type", "text");
+}
+pinToText();
