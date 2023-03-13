35daed0cc5b6d6c520bda79953abba2d6e81bd9c
Andrei <andrei.nicolas@hotmail.com>
2023-03-13 16:35:40 +1300

    Update solution

--- work examples/Auto-fill takes Email input as username/script.js
+++ work examples/Auto-fill takes Email input as username/script.js
@@ -1,20 +1,20 @@
 //Issue Firefox Auto-fill takes Email input as username instead of Username as Username in Form Submission
 
-// Solution is to Take the Username input with value and slot it right before the password
+// Solution is to Take the Username input with value and slot it right before the email
 
 
 //This is to prevent autofill from taking Email as Username
-const cloneandInsertUsername = function (){
+var cloneandInsertUsername = function (){
   
-    const usernameElem = document.querySelector('#newSignInName');
-    const passwordElem = document.querySelector('#newResetPassword');
+  const usernameElem = document.querySelector('#newSignInName');
+  const emailElem = document.querySelector('#email');
+ 
+   let cloneElem = usernameElem.cloneNode(true);
    
-     const cloneElem = usernameElem.cloneNode(true)
-     
-     cloneElem.id = 'newSignInNameHidden';
-     cloneElem.setAttribute("hidden", true);
-     passwordElem.before(cloneElem);
-   
-   }
-   
-   cloneandInsertUsername();
+   cloneElem.id = 'newSignInNameHidden';
+   cloneElem.setAttribute("hidden", true); 
+   emailElem.before(cloneElem);
+ 
+ }
+ 
+ cloneandInsertUsername();
