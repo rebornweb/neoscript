715b8dfcd93f22149e3628a724c1a513a6cd5988
andrei <andrei.nicolas@unifysolutions.net>
2023-04-11 14:58:48 +1200

    C# basic unit tests

---
+++ work examples/csharp/UnrealTest/UnrealTest/ultclass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UnrealTest
+{
+    internal class ultclass
+    {
+        [TestFixture]
+        public class MyTests
+        {
+            [Test]
+            public void TestAddition()
+            {
+                int result = Calculator.Add(30, 3);
+                Assert.AreEqual(33, result);
+            }
+        }
+
+        public static class Calculator
+        {
+            public static int Add(int a, int b)
+            {
+                return a + b ;
+            }
+        }
+    }
+}
