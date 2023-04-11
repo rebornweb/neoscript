d7dde6a3bb92a24f2e92b6d5573bde27fbe31aa7
andrei <andrei.nicolas@unifysolutions.net>
2023-04-11 14:58:48 +1200

    C# basic unit tests

---
+++ work examples/csharp/UnrealTest/UnrealTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnrealTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
