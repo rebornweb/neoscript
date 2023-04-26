a04a98cfe188564419f81cb976a782d79cebf654
andrei <andrei.nicolas@unifysolutions.net>
2023-04-26 12:52:43 +1200

    Addition of ReactJS form mockups

---
+++ work examples/js/reactjs/test-form-app/src/mockupData.js
@@ -0,0 +1,20 @@
+// mockupData.js
+
+const mockupData = [
+    {
+      id: 1,
+      name: "John",
+      age: 25,
+      email: "john@example.com"
+    },
+    {
+      id: 2,
+      name: "Alice",
+      age: 30,
+      email: "alice@example.com"
+    },
+    // ... more mockup data
+  ];
+  
+  export default mockupData;
+  
