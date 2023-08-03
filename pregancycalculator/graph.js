d5db7345e0805bd728f346ad18c217ff709798ff
Andrei <andrei.nicolas@hotmail.com>
2023-08-03 18:15:37 +1000

    Pregnancy Calculator

---
+++ pregancycalculator/graph.js
@@ -0,0 +1,122 @@
+
+//New Graph Code
+
+var yum = angular.module('ui.bootstrap.demo', ['ngAnimate', 'ngSanitize', 'ui.bootstrap', 'chart.js']);
+
+yum.controller('ModalDemoCtrl', function($uibModal, $log, $document) {
+var $ctrl = this;
+$ctrl.animationsEnabled = true;
+$ctrl.open = function(size, parentSelector) {
+var parentElem = parentSelector ?
+angular.element($document[0].querySelector('.modal-demo ' + parentSelector)) : undefined;
+var modalInstance = $uibModal.open({
+  animation: $ctrl.animationsEnabled,
+  ariaLabelledBy: 'modal-title',
+  ariaDescribedBy: 'modal-body',
+  templateUrl: 'myModalContent.html',
+  controller: 'ModalInstanceCtrl',
+  controllerAs: '$ctrl',
+  size: size,
+  appendTo: parentElem,
+  resolve: {
+    items: function() {
+      return $ctrl.items;
+    }
+  }
+});
+
+modalInstance.result.then(function(selectedItem) {
+  $ctrl.selected = selectedItem;
+  }, function() {
+    $log.info('Modal dismissed at: ' + new Date());
+  });
+};
+
+$ctrl.openComponentModal = function() {
+  var modalInstance = $uibModal.open({
+  animation: $ctrl.animationsEnabled,
+  component: 'modalComponent',
+  resolve: {
+    items: function() {
+      return $ctrl.items;
+    }
+  }
+});
+
+modalInstance.result.then(function(selectedItem) {
+    $ctrl.selected = selectedItem;
+  }, function() {
+    $log.info('modal-component dismissed at: ' + new Date());
+  });
+};
+});
+
+//Call Graph
+angular.module('ui.bootstrap.demo').controller('ModalInstanceCtrl', function($uibModalInstance, $scope) {
+  var $ctrl = this;
+
+  $scope.labels = ['IUD Availablity'];
+  $scope.series = ['Period Length',  'Beginning of Ovulation', 'IUD End', 'Full Cycle'];
+
+
+var html = document.getElementById("datHolder");
+var div = document.createElement("div");
+div.innerHTML = html;
+
+
+
+
+var expovu = document.getElementById("expectovu").value;
+
+var iudEnd = document.getElementById("iudend").value;
+var fullC = document.getElementById("fullc").value;
+  $scope.data = [
+
+    [expovu],
+    [iudEnd],
+    [fullC],
+  ];
+  $scope.options = {
+    legend: {
+    display: true
+  }
+};
+
+  $ctrl.ok = function() {
+    $uibModalInstance.close();
+  };
+
+  $ctrl.cancel = function() {
+    $uibModalInstance.dismiss('cancel');
+  };
+});
+
+//console.log(angular.element('#username')[0].value);
+angular.module('ui.bootstrap.demo').component('modalComponent', {
+  templateUrl: 'myModalContent.html',
+  bindings: {
+    resolve: '<',
+    close: '&',
+    dismiss: '&'
+  },
+  controller: function() {
+    var $ctrl = this;
+    $ctrl.$onInit = function() {
+      $ctrl.items = $ctrl.resolve.items;
+      $ctrl.selected = {
+      item: $ctrl.items[0]
+    };
+  };
+  $ctrl.ok = function() {
+    $ctrl.close({
+      $value: $ctrl.selected.item
+    });
+  };
+
+$ctrl.cancel = function() {
+  $ctrl.dismiss({
+    $value: 'cancel'
+  });
+};
+}
+});
---
+++ pregancycalculator/readme.md
@@ -0,0 +1,30 @@
+Refresher
+
+Module Definitions:
+
+// Define the rootApp module and inject firstApp and secondApp as dependencies
+var rootApp = angular.module('rootApp', ['firstApp', 'secondApp']);
+// Define the firstApp module and inject dependencies such as ngAnimate, ngSanitize, and ui.bootstrap
+var firstApp = angular.module('firstApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+// Define the secondApp module and inject ngMaterial as a dependency
+var secondApp = angular.module('secondApp', ['ngMaterial']);
+Controllers:
+a. hourController: This controller is associated with the firstApp module and handles date manipulation and calculations.
+
+b. prangeController: This controller is associated with the secondApp module and appears to handle some date range calculations.
+
+c. AppCtrl: This controller is associated with the secondApp module and appears to handle date-related functionality.
+
+d. rootCon: This controller is associated with the rootApp module, but its contents are not visible in the code snippet you provided.
+
+Configurations:
+The secondApp module has a configuration block that configures date formatting and parsing using $mdDateLocaleProvider from Angular Material.
+
+HTML and UI:
+The code references various HTML elements and classes like datetimepicker1, fullc, prangeC, etc. These elements seem to be part of the user interface (UI) and interact with the AngularJS controllers.
+
+40 weeks (280 days from the LMP) is 9 months Estimated due date March 1st to December
+
+Example:
+Last LMP 10 Jan
+UPSI 22 and 28
