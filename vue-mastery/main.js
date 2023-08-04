1e986d9c443ad5c80210c4ce7a92b1ed466d97ad
Andrei <andrei.nicolas@hotmail.com>
2023-08-04 17:44:39 +1000

    Vue Mastery

---
+++ vue-mastery/main.js
@@ -0,0 +1,12 @@
+// Start from https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding
+const app = Vue.createApp({
+    data() {
+        return {
+            products:{ product:'Socks',description: 'Clean'}
+            
+        }
+
+    }
+
+
+});
