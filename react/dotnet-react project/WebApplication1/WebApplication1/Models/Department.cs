a2d64a9503b5d816f8cb35b5c8149391f85ee1f0
Andrei <andrei.nicolas@hotmail.com>
2023-08-12 20:40:10 +1000

    Update solutions and git ignore

---
+++ react/dotnet-react project/WebApplication1/WebApplication1/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+[Table("Department")]
+
+public class Department
+{
+    [Key]
+    public int DepartmentId { get; set; }
+
+    public string DepartmentName { get; set; }
+}
---
+++ react/dotnet-react project/WebApplication1/WebApplication1/Repository/APIDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+public class APIDbContext : DbContext
+{
+    public APIDbContext(DbContextOptions<APIDbContext> options) : base(options) { }
+    public DbSet<Department> Departments
+    {
+        get;
+        set;
+    }
+    public DbSet<Employee> Employees
+    {
+        get;
+        set;
+    }
+}
