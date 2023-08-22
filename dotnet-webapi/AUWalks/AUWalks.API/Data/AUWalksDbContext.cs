3b2ba884ad0bc93123225ddd680e5f26649c246f
Andrei <andrei.nicolas@unifysolutions.net>
2023-08-22 17:46:03 +1000

    Update start here

---
+++ dotnet-webapi/AUWalks/AUWalks.API/Data/AUWalksDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AUWalks.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AUWalks.API.Data
+{
+    public class AUWalksDbContext : DbContext
+    {
+        public AUWalksDbContext(DbContextOptions dbContextOptions): base(dbContextOptions)
+        {
+                
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+
+    }
+}
