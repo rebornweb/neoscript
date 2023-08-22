f36c9648a83ac515660fa6d440f3b14cad615d5e
Andrei <andrei.nicolas@unifysolutions.net>
2023-08-22 13:41:24 +1000

    Start of dot net project webapi

---
+++ dotnet-webapi/AUWalks/AUWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AUWalks.API.Controllers
+{
+
+    // https://localhost:port/api/students
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET Request https://localhost:port/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] {
+                "Jane", "John"
+            };
+
+            return Ok(studentNames);
+        }
+    }
+}
