9d067d4cf5eed8ff4a633b6af9b390f980a9df33
Andrei <andrei.nicolas@unifysolutions.net>
2023-08-23 17:10:08 +1000

    Update - Regions data, Migration tables DB

---
+++ dotnet-webapi/AUWalks/AUWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,40 @@
+﻿using AUWalks.API.Models.Domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AUWalks.API.Controllers
+{   
+    // api/regions
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {   
+        //GET: URL:port/api/regions
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = new List<Region> { 
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Sydney Region",
+                    Code = "SYD",
+                    RegionImageUrl = "https://cdn.pixabay.com/photo/2014/06/06/09/36/sydney-opera-house-363244_1280.jpg"
+
+                },
+                new Region
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Brisbane Region",
+                    Code = "BNE",
+                    RegionImageUrl = "https://cdn.pixabay.com/photo/2016/02/25/13/35/beach-1222080_1280.jpg"
+
+                }
+
+            };
+
+            return Ok(regions);
+        }
+
+    }
+}
