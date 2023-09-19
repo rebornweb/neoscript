0cfee127bf0301366228badcc9c744dcd2ba3d99
Andrei <andrei.nicolas@unifysolutions.net>
2023-09-19 18:12:08 +1000

    Vue L6

--- vue-mastery/main.js
+++ vue-mastery/main.js
@@ -1,11 +1,17 @@
-//https://www.vuemastery.com/courses/intro-to-vue-3/list-rendering-vue3
+//Start at level 6
+
 const app = Vue.createApp({
     data() {
         return {
+            cart:0,
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+            ]
         }
     }
 })
