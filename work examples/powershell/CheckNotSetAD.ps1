c8ecd9a8c42c50062eb236e25778d58eb5d21e93
Andrei <andrei.nicolas@unifysolutions.net>
2023-11-24 10:39:44 +1000

    AD Scripts

--- work examples/powershell/CheckNotSetAD.ps1
+++ work examples/powershell/CheckNotSetAD.ps1
@@ -1,19 +1,32 @@
-# Import the Active Directory module
 Import-Module ActiveDirectory
 
-$attribute = "accountNameHistory"
+$attribute = "extensionAttribute3"
 $OU = "OU=Users,OU=Company,DC=Domain,DC=local"
 $timer = [Diagnostics.Stopwatch]::StartNew()
 
-$users = Get-ADUser -Filter "$attribute -notlike '*'" -searchbase $OU -properties $attribute
+#New Powershell
+$users = Get-ADUser -Filter { -not (extensionAttribute3 -like '*') -or extensionAttribute3 -eq $null } -SearchBase $OU -Properties $attribute
+
+#Old
+#$users = Get-ADUser -Filter { -not (extensionAttribute3 -like '*')} -SearchBase $OU -Properties $attribute
+
+$countWithValue = 0
+$countWithoutValue = 0
 
 if ($users) {
     $users | ForEach-Object {
-        Write-Host "Found User $($_.SamAccountName) when searching Attribute $attribute"
+        if ($null -eq $_.extensionAttribute3) {
+            Write-Host "Found User $($_.SamAccountName) with no value for Attribute $attribute"
+            $countWithoutValue++
+        } else {
+            Write-Host "Found User $($_.SamAccountName) with value $($user.extensionAttribute3) for Attribute $attribute"
+            $countWithValue++
+        }
     }
-    Write-Host "Total number of users found: $($users.Count)"
+    Write-Host "Total number of users with value for Attribute $attribute: $countWithValue"
+    Write-Host "Total number of users without value for Attribute $attribute: $countWithoutValue"
 } else {
-    Write-Host "No users found with non-null value for Attribute $attribute"
+    Write-Host "No users found with null or empty value for Attribute $attribute"
 }
 
 Write-Host "Elapsed time: $($timer.Elapsed.TotalSeconds) seconds."
