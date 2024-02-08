eb5bac947511a18c048b2ffdfac8fdcea4665d3e
Andrei <andrei.nicolas@hotmail.com>
2024-02-08 22:34:09 +1000

    New working API

---
+++ dotnet-webapi/open_shop/WebApplication1/WebApplication1/Controllers/ApiController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic; // Required for List<T>
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient; // For .NET Core
+// If you're using .NET Framework, you would use System.Data.SqlClient instead
+
+[Route("api/[controller]")]
+[ApiController]
+public class StoredProceduresController : ControllerBase
+{
+    private readonly string _connectionString;
+
+    public StoredProceduresController(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    [HttpGet]
+    public IActionResult GetStoredProcedures()
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            connection.Open();
+            var commandText = "SELECT name AS ProcedureName, create_date AS CreateDate FROM sys.procedures ORDER BY name";
+
+            using (var command = new SqlCommand(commandText, connection))
+            {
+                using (var reader = command.ExecuteReader())
+                {
+                    var procedures = new List<Dictionary<string, object>>();
+
+                    while (reader.Read())
+                    {
+                        var procedure = new Dictionary<string, object>();
+                        for (var i = 0; i < reader.FieldCount; i++)
+                        {
+                            procedure[reader.GetName(i)] = reader[i];
+                        }
+                        procedures.Add(procedure);
+                    }
+
+                    return Ok(procedures);
+                }
+            }
+        }
+    }
+}
---
+++ dotnet-webapi/open_shop/WebApplication1/WebApplication1/Controllers/TestConnectionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+
+namespace YourNamespace.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConnectionTestController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConnectionTestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult TestConnection()
+        {
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    return Ok("Connection successful.");
+                }
+                catch (Exception ex)
+                {
+                    return StatusCode(500, $"Connection failed: {ex.Message}");
+                }
+            }
+        }
+    }
+}
